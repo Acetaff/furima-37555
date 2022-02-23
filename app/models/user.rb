12d40f00d99906856585864f5c84768097c2663f
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-23 21:02:23 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
+  
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には半角英数を両方含む必要があります'}
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_kana, presence: true
-  validates :first_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶ]+\z/, message: 'に全角カタカナを使用してください'} do
+    validates :last_kana
+    validates :first_kana
+  end
+
   validates :birthday, presence: true
 end
