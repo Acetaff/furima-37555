e0448d63e41411a2b55b112e1ec55ffca59b76c7
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-24 10:28:17 +0900

    context「登録できる」を確認

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password { 'abc123' }
     password_confirmation {password}
-    last_name {Faker::Name.last_name}
-    first_name {Faker::Name.first_name}
-    last_kana {Faker::Name.last_name}
-    frist_kana {Faker::Name.frist_name}
+    last_name { 'てすと' }
+    first_name { 'まん' }
+    last_kana { 'テスト' }
+    first_kana { 'マン' }
     birthday {Faker::Date.birthday}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+    
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "全ての項目が正しく存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない場合' do
+
+    end
+  end
 end
