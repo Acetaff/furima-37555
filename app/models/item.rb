e27dd9edaa0f1729fcd2a500f7e2b4354c61ddfd
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-25 16:21:35 +0900

    バリデーションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,16 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
+  end
+
+  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :status_id
     validates :burden_id
     validates :prefecture_id
     validates :shipping_day_id
-    validates :price, format: { with: /\A[300-9999999]+\z/, message: 'には(￥300〜￥9,999,999)までの"半角数字"を入力してください'}
   end
+
+  validates :price, presence: true, format: { with: /\A[300-9999999]+\z/, message: 'には(￥300〜￥9,999,999)までの"半角数字"を入力してください'}
+
 end
