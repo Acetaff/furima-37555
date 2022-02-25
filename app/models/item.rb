4cd30c1fb9927f943555e56bf7d92e6e71422cee
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-25 16:10:56 +0900

    itemモデルと各クラスのアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :shipping_day_id
 
   with_options presence: true do
     validates :name
