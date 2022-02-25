36b666925670679e04a47886965594fe6baa1aa0
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-25 14:45:41 +0900

    ビューファイルを修正＋現在不要な記述をコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
 
   private
 
-  def item_params
-    params.require(:item)permit(:name, :description, :category_id, :status_id, :burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
-  end
+  # def item_params
+  #   params.require(:item)permit(:name, :description, :category_id, :status_id, :burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  # end
 
 
 end
