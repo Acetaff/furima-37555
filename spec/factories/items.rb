e3683a240fef1cd005595d031538a928b0d95f53
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-28 12:39:21 +0900

    FactoryBotを設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    description {Faker::Lorem.sentence}
+    category_id {Ctegory.id}
+    status_id {Status.id}
+    burden_id {Burden.id}
+    prefecture_id {Prefecture.id}
+    shipping_day_id {ShippingDay.id}
+    price {Random.rand(300..9999999)}
+    association :user
   end
 end
