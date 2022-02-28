71c4231ed10ef44382dc509ad35375c207960c50
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-02-28 14:25:45 +0900

    context「出品できる」を確認＋FactoryBotを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,16 @@ FactoryBot.define do
   factory :item do
     name {Faker::Lorem.sentence}
     description {Faker::Lorem.sentence}
-    category_id {Ctegory.id}
-    status_id {Status.id}
-    burden_id {Burden.id}
-    prefecture_id {Prefecture.id}
-    shipping_day_id {ShippingDay.id}
+    category_id {Random.rand(2..11)}
+    status_id {Random.rand(2..7)}
+    burden_id {Random.rand(2..3)}
+    prefecture_id {Random.rand(2..48)}
+    shipping_day_id {Random.rand(2..4)}
     price {Random.rand(300..9999999)}
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '出品できる場合'
+      it '全ての項目が正しく存在していれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
 end
