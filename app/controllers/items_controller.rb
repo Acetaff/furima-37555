16c7f6abeb9ec62444d4d547c1ff8c1f7f1ebdf5
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-01 18:03:30 +0900

    DRYの原則に則り、コードを整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,10 +41,13 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index 
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
