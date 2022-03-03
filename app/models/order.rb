685d3e36faea633863ac41586ba2bff983a5427a
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-03 18:12:10 +0900

    attr_accessorを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order
   include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num, :user_id, :item_id, :product_purchase_id
 end
