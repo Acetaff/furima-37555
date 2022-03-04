0996359f6766084d01bce4317efaccd4fda6add2
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-04 16:52:44 +0900

    JSのname属性を修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,10 +8,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("number"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
-      cvc: formData.get("cvc"),
+      number: formData.get("order[number]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+      cvc: formData.get("order[cvc]"),
     };
 
     Payjp.createToken(card, (status, response) => {
