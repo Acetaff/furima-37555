977a1f2f1d6989734ee32b2207ff22834d00a3f5
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-04 11:46:23 +0900

    form_withにパスを設定＋バリデーションを修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,11 +2,6 @@ class Order
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num, :user_id, :item_id, :product_purchase_id
 
-  with_options prefecture: true do
-    validates :user_id
-    validates :item_id
-  end
-
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
