9e8d19156592cdf8d730a1ae6ade99c8a1095e24
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-04 14:49:04 +0900

    phone_numのバリデーションを修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :municipalities
     validates :address
-    validates :phone_num, format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "is invalid. hyphen(-)"}
+    validates :phone_num, format: {with: /\A[0-9]{10,11}\z/, message: "には１０桁以上・11桁以内の半角数字を入力して下さい"}
     validates :product_purchase_id
     validates :user_id
     validates :item_id
