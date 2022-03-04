0c73c4489b1825de59f41e55ea7b4fa740f20d09
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-04 12:26:06 +0900

    クレジットカード決済処理を記述＋ストロングパラメーターに追記

--- app/controllers/product_purchases_controller.rb
+++ app/controllers/product_purchases_controller.rb
@@ -8,6 +8,7 @@ class ProductPurchasesController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -18,7 +19,16 @@ class ProductPurchasesController < ApplicationController
   private
 
   def order_params
-    params.reqire(:order).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num, :item_id).merge(user_id: current_user.id)
+    params.reqire(:order).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num, :item_id).merge(user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
   
 end
