aae10044fb3a239b18471f30243407e1b08c07a9
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-04 11:36:30 +0900

    createアクションに保存の処理を記述＋ストロングパラメーターを設定

--- app/controllers/product_purchases_controller.rb
+++ app/controllers/product_purchases_controller.rb
@@ -2,9 +2,23 @@ class ProductPurchasesController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.reqire(:order).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num, :item_id).merge(user_id: current_user.id)
   end
   
 end
