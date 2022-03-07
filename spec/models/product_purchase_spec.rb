0a5f7b0737e5d0b8e26fb149250097c3eef9aa20
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-07 12:21:21 +0900

    エンティティをリストアップ

--- spec/models/product_purchase_spec.rb
+++ spec/models/product_purchase_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ProductPurchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product_purchase = FactoryBot.build(:product_purchase)
+  end
+
+  describe '購入情報の保存' do
+    
+    context '保存できる場合' do
+      it 'userとitemの情報が紐づいていれば保存できる' do
+        expect(@product_purchase).to be_valid
+      end
+    end
+
+    context '保存できない場合' do
+      it 'user情報が紐づいていなければ保存できない' do
+        
+      end
+      it 'item情報が紐づいていなければ保存できない' do
+        
+      end
+    end
+  end
 end
