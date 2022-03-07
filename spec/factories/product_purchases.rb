757c969294e94266b13c873ed10702b63917237c
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-07 12:06:10 +0900

    FactoryBotを設定

--- spec/factories/product_purchases.rb
+++ spec/factories/product_purchases.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :product_purchase do
-    
+    association :user
+    association :item
+    association :purchaser
   end
 end
--- spec/factories/purchasers.rb
+++ spec/factories/purchasers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchaser do
-    
+    post_code {123-4567}
+    prefecture {Random.rand(2..48)}
+    municipalities {Faker::Address.name}
+    address {Faker::Address.name}
+    building_name {Fakser::Address.name}
+    phone_num {Faker::Number.number(11,12)}
+    association :product_purchase
   end
 end
