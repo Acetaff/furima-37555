81413cb4cd23db791d58e4a0d467e5a43467d26b
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-07 14:54:33 +0900

    エンティティをリストアップ

--- spec/models/purchaser_spec.rb
+++ spec/models/purchaser_spec.rb
@@ -11,9 +11,51 @@ RSpec.describe Purchaser, type: :model do
       it '必要な項目が正しく存在していれば保存できる' do
         expect(@purchaser).to be_valid
       end
-      it '建物名が空でも保存できる' do
+      it 'building_nameが空でも保存できる' do
         @purchaser.building_name = ''
         expect(@purchaser).to be_valid
+      end
+    end
+
+    context '保存できない' do
+      it 'post_codeが空では保存できない' do
+        
+      end
+      it 'post_codeが全角数字では保存できない' do
+        
+      end
+      it 'post_codeが数字以外では保存できない' do
+        
+      end
+      it '-（ハイフン）が必要な位置に入っていなければ保存できない' do
+        
+      end
+      it 'prefecture_idが空では保存できない' do
+        
+      end
+      it 'prefecture_idが１（初期値”ーー”）では保存できない' do
+        
+      end
+      it 'municipalitiesが空では保存できない' do
+        
+      end
+      it 'addressが空では保存できない' do
+        
+      end
+      it 'phone_numが空では保存できない' do
+        
+      end
+      it 'phone_numが全角数字では保存できない' do
+        
+      end
+      it 'phone_numが数字以外では保存できない' do
+        
+      end
+      it 'phone_numが１０桁未満では保存できない' do
+        
+      end
+      it 'phone_numが１２桁以上では保存できない' do
+        
       end
     end
 
