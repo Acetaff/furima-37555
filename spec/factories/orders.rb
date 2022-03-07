669a57b586f93af73d23c5f067ea210b97cace98
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-07 18:39:36 +0900

    context「保存できない」を確認

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :order do
     post_code {"123-4567"}
     prefecture_id {Random.rand(2..48)}
-    municipalities {Faker::Address.name}
-    address {Faker::Address.name}
-    building_name {Faker::Address.name}
+    municipalities {"某市"}
+    address {"某所"}
+    building_name {"何某"}
     phone_num {Faker::Number.leading_zero_number(digits:10)}
     token {Faker::Crypto.sha1}
   end
