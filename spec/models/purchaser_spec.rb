936f1c6f2f2719f9a263aefdc346f3ff7f7f803e
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-07 14:36:19 +0900

    context 「保存できる」を確認

--- spec/models/purchaser_spec.rb
+++ spec/models/purchaser_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Purchaser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchaser = FactoryBot.build(:purchaser)
+  end
+
+  describe '配送先情報の保存' do
+    
+    context '保存できる場合' do
+      it '必要な項目が正しく存在していれば保存できる' do
+        expect(@purchaser).to be_valid
+      end
+      it '建物名が空でも保存できる' do
+        @purchaser.building_name = ''
+        expect(@purchaser).to be_valid
+      end
+    end
+
+  end
 end
